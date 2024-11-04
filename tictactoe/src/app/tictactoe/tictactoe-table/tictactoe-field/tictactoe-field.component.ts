81535e609303ee365800ab269f42e549441cef6a
Paul Schein <p.schein@students.htl-leonding.ac.at>
2024-11-04 10:57:19 +0100

    added components

---
+++ tictactoe/src/app/tictactoe/tictactoe-table/tictactoe-field/tictactoe-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tictactoe-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './tictactoe-field.component.html',
+  styleUrl: './tictactoe-field.component.scss'
+})
+export class TictactoeFieldComponent {
+
+}
---
+++ tictactoe/src/app/tictactoe/tictactoe-table/tictactoe-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tictactoe-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './tictactoe-table.component.html',
+  styleUrl: './tictactoe-table.component.scss'
+})
+export class TictactoeTableComponent {
+
+}
